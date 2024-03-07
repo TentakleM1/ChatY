e95e3687075434d364398267d8c28b1c033a632a
TentakleM1 <124381778+TentakleM1@users.noreply.github.com>
2024-03-07 20:56:56 +0300

    Add files via upload

---
+++ dist/db/data.ts
@@ -0,0 +1,68 @@
+
+type Data = {
+    chats: Record<string, any>[],
+    profile: Record<string, any>
+}
+
+export const data: Data = {
+    chats: [
+        {
+            profile_name: 'Andray',
+            messages: [
+                {
+                    author: 'you',
+                    message: 'Привет',
+                    date: '12:30'
+                },
+                {
+                    author: 'Andray',
+                    message: 'Хэй!) Рад тебя слышать чувааааак!',
+                    date: '12:35'
+                }
+            ],
+            newMessage: 1
+        },
+        {
+            profile_name: 'Sofia',
+            messages: [
+                {
+                    author: 'Sofia',
+                    message: 'Где моя зажигалка ?',
+                    date: '18:30'
+                },
+                {
+                    author: 'you',
+                    message: 'Хз, я тебе её отдавал еще на перерыве, как вернулся',
+                    date: '19:30'
+                }
+            ],
+            newMessage: 0
+        },
+        {
+            profile_name: 'Gina',
+            messages: [
+                {
+                    author: 'you',
+                    message: 'Пока',
+                    date: '12:30'
+                },
+                {
+                    author: 'Gina',
+                    message: 'Ну давай',
+                    date: '12:35'
+                }
+            ],
+            newMessage: 1
+        }
+    ],
+    profile: {
+        first_name: 'Олег',
+        second_name: 'Рыгыль',
+        login: 'oleg',
+        email: 'olg@mail.ru',
+        phone: '+73498354443',
+        password: 'hE3ugrskd'
+
+    }
+}
+
