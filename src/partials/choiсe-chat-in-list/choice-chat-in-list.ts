234216d3299b8021fc10b99b8eca1d80fa68cc88
TentakleM1 <124381778+TentakleM1@users.noreply.github.com>
2024-03-08 21:49:38 +0300

    Update choice-chat-in-list.ts

--- src/partials/choiсe-chat-in-list/choice-chat-in-list.ts
+++ src/partials/choiсe-chat-in-list/choice-chat-in-list.ts
@@ -1,5 +1,5 @@
-import { Block } from '../../core/block.ts';
-import { template } from './choiсe-chat-in-list.tmpl.ts';
+import { Block } from '../../core/block';
+import { template } from './choiсe-chat-in-list.tmpl';
 
 export default class ChoiceChat extends Block {
   constructor(props: Record<string, any> = {}) {
