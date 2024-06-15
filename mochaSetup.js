9869f52f8ea64d1fd8f41f11c7ff6e5d7c47c025
TentakleM1 <tentakelme7@gmail.com>
2024-06-15 15:27:09 +0300

    fix module

--- mochaSetup.js
+++ mochaSetup.js
@@ -1,9 +1,6 @@
 import { JSDOM } from 'jsdom';
 
-const { window } = new JSDOM('<div id="app"></div>', {
-    url: 'http://localhost:5173',
-  });
-  
-  global.window = window;
-  global.document = window.document;
-  global.DocumentFragment = window.DocumentFragment;
+const jsdom = new JSDOM(`<body></body>`);
+
+global.window = jsdom.window;
+global.document = jsdom.window.document;
